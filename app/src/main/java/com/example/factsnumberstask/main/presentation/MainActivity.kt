136ad4d580cc9a98802e4c687e21a4ec5a1f4c58
Malik <bittiev.malik@mail.ru>
2025-02-21 01:19:45 +0300

    initial commit

---
+++ app/src/main/java/com/example/factsnumberstask/main/presentation/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.factsnumberstask.main.presentation
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.factsnumberstask.R
+import com.example.factsnumberstask.numbers.presentation.NumbersFragment
+
+class MainActivity : AppCompatActivity(), ShowFragment {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        if (savedInstanceState == null)
+            show(NumbersFragment(),false)
+    }
+
+    override fun show(fragment: Fragment) {
+        show(fragment, true)
+    }
+
+    private fun show(fragment: Fragment, add: Boolean) {
+        //todo
+        val transaction = supportFragmentManager.beginTransaction()
+        val conteiner = R.id.container
+        if (add) {
+            transaction.addToBackStack(fragment.javaClass.simpleName)
+        } else {
+            transaction.replace(conteiner, fragment)
+        }
+        transaction.commit()
+    }
+
+}
+
+interface ShowFragment {
+
+    fun show(fragment: Fragment)
+    class Empty() : ShowFragment {
+        override fun show(fragment: Fragment) = Unit
+
+    }
+}
---
+++ app/src/main/java/com/example/factsnumberstask/numbers/presentation/NumbersFragment.kt
@@ -0,0 +1,47 @@
+package com.example.factsnumberstask.numbers.presentation
+
+import android.content.Context
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ProgressBar
+import androidx.fragment.app.Fragment
+import com.example.factsnumberstask.R
+import com.example.factsnumberstask.details.presentation.DetailsFragment
+import com.example.factsnumberstask.main.presentation.ShowFragment
+
+class NumbersFragment : Fragment() {
+
+    private var showFragment: ShowFragment = ShowFragment. Empty()
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        showFragment = context as ShowFragment
+    }
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragmet_numbers, container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<ProgressBar>(R.id.progressbar).visibility =View.GONE
+        view.findViewById<View>(R.id.getFact).setOnClickListener{
+        val detailsFragment = DetailsFragment.newInstance("some information for number")
+
+            //переход на след фрагмент
+            showFragment.show(detailsFragment)
+            }
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+            showFragment = ShowFragment.Empty()
+    }
+
+}
